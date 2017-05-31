2f34d782e80ea2e35c1ff13240df39431f9ac362
james golden <jamesgolden1@gmail.com>
2017-05-30 19:43:01 -0700

    added reconstruction to t_primaObject

--- tutorials/t_primaObject.m
+++ tutorials/t_primaObject.m
@@ -1,18 +1,13 @@
 % t_primaObject
-
-% Generate stimulus
-% Build primaArray
-%   pseudo cMosaic
-%   electrode array
-%   electrode activation
-%   electrode threshold
-%   bipolar activation
-%   rgc spikes
 % 
-% return bp, ir
+% The Prima object allows for the simulation of a subretinal prosthesis.
+% The activation of the photovoltaic electrode array in response to a movie
+% stimulus is generated. The bipolar current and RGC spikes are generated
+% in response to the electrode stimulation. 
 
-% do recon
-
+% With the RGC spikes, and a retina with a fixed set of parameters, we can
+% generate the stimulus reconstruction to examine how it is affected by the
+% prosthetic stimulation.
 
 %% Load stimulus movie
 
@@ -28,8 +23,11 @@ for ii = 1:szFrames
 end
 
 % movieIn = rand(100,100,10);
-movieIn = hallMovieResize(:,:,1:5);
-%%
+movieIn = hallMovieResize(:,:,1:600);
+
+clear vidFrame hallMovieResize
+
+%% Simulate bipolar and RGC response to prosthesis stimulation
 
 primaParams.pixelWidth = .5*35e-6; % meters
 primaParams.ecc = 1.8;       % mm
@@ -43,8 +41,30 @@ primaRecon = primaArray(movieIn,primaParams)
 
 primaRecon.compute(movieIn)
 
-% primaRecon.computeBipolar()
-% 
-% primaRecon.computeRGC()
+% Make a recon object here? Maybe primaRecon.reconstruct()?
+
+%% Reconstruct - get spikes and decoding filter
+
+spikeResp = mosaicSpikes(primaRecon.innerRetina);
+
+rd = RdtClient('isetbio');
+rd.crp('/resources/data/istim');
+filterFile = 'filters_mosaic0_sv75_w1_sh2_may26primaSmall.mat';
+data  = rd.readArtifact(filterFile(1:end-4), 'type', 'mat');
+filterMat = data.filterMat; clear data;
+
+%% Generate reconstructed movie
+spikeAug(1,:) = ones(1,size(spikeResp,2));
+spikeAug(2:9807,:) = spikeResp;
+movRecon = filterMat'*spikeAug;
+movReconPlay = reshape(movRecon,[100 100 size(spikeResp,2)]);
+nFramesPlay = 40;
+figure; ieMovie(movReconPlay(:,:,1:nFramesPlay));
 
 
+%% Spatial zeroing of filter
+% lambda = .01;
+% filterMat2 = zeroFilter(filterMat,lambda);
+% movRecon2 = filterMat2'*spikeAug;
+% movReconPlay2 = reshape(movRecon2,[100 100 size(spikeResp,2)]);
+% figure; ieMovie(movReconPlay2(:,:,1:nFramesPlay));
