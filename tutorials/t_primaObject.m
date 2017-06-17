535fe2ff419102b6849adec1aa3b8a78d8aafa26
james golden <jamesgolden1@gmail.com>
2017-06-16 19:59:18 -0700

    new decoding filter with fixed electrode activation, no DSoff

--- tutorials/t_primaObject.m
+++ tutorials/t_primaObject.m
@@ -39,29 +39,30 @@ primaRecon.compute(movieIn)
 
 %% Reconstruct - get spikes and decoding filter
  
-% spikeResp = mosaicSpikes(primaRecon.innerRetina);
-% % save('spikeResp_hallway.mat','spikeResp');
-% 
-% % Remote data toolbox - download decoding filter
-% rd = RdtClient('isetbio');
-% rd.crp('/resources/data/istim');
-% % filterFile = 'filters_mosaic0_sv75_w1_sh2_may26primaSmall';
+spikeResp = mosaicSpikes(primaRecon.innerRetina);
+% save('spikeResp_hallway.mat','spikeResp');
+
+% Remote data toolbox - download decoding filter
+rd = RdtClient('isetbio');
+rd.crp('/resources/data/istim');
+% filterFile = 'filters_mosaic0_sv75_w1_sh2_may26primaSmall';
 % filterFile = 'filters_mosaic0_sv20_w1_sh2_dr0';
-% data  = rd.readArtifact(filterFile, 'type', 'mat');
-% filterMat = data.filterMat; clear data;
-% 
-% % % %% Generate reconstructed movie
-% spikeAug(1,:) = ones(1,size(spikeResp,2));
-% spikeAug(1+[1:size(spikeResp,1)],:) = spikeResp;
-% movRecon = filterMat'*spikeAug;
-% movReconPlay = reshape(movRecon,[100 100 size(spikeResp,2)]);
-% nFramesPlay = stimFrames;
-% figure; ieMovie(movReconPlay(:,:,1:nFramesPlay));
-% 
-% 
-% % %% Spatial zeroing of filter
-% % lambda = .01;
-% % filterMat2 = zeroFilter(filterMat,lambda);
-% % movRecon2 = filterMat2'*spikeAug;
-% % movReconPlay2 = reshape(movRecon2,[100 100 size(spikeResp,2)]);
-% % figure; ieMovie(movReconPlay2(:,:,1:nFramesPlay));
+filterFile = 'filters_mosaic0_sv10_w1_sh2_dr0';
+data  = rd.readArtifact(filterFile, 'type', 'mat');
+filterMat = data.filterMat; clear data;
+
+% % %% Generate reconstructed movie
+spikeAug(1,:) = ones(1,size(spikeResp,2));
+spikeAug(1+[1:size(spikeResp,1)],:) = spikeResp;
+movRecon = filterMat'*spikeAug;
+movReconPlay = reshape(movRecon,[100 100 size(spikeResp,2)]);
+nFramesPlay = stimFrames;
+figure; ieMovie(movReconPlay(:,:,1:nFramesPlay));
+
+
+% %% Spatial zeroing of filter
+% lambda = .01;
+% filterMat2 = zeroFilter(filterMat,lambda);
+% movRecon2 = filterMat2'*spikeAug;
+% movReconPlay2 = reshape(movRecon2,[100 100 size(spikeResp,2)]);
+% figure; ieMovie(movReconPlay2(:,:,1:nFramesPlay));
